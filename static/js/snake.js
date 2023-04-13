042bdffbe31848fb69b9a9c2ab8c1984938b07f9
benngim <102727152+benngim@users.noreply.github.com>
2023-04-13 23:36:57 +1000

    Added retry and instructions for snake game

--- static/js/snake.js
+++ static/js/snake.js
@@ -1,26 +1,27 @@
 /* Implements Snake Game */
 
-// Board Variables
+// Board variables
 var blocksize = 25;
 var rows = 20;
 var cols = 20;
 var board;
 var context;
 
-// Snake
-var snakeX = blocksize * 5;
-var snakeY = blocksize * 5;
-var velocityX = 0;
-var velocityY = 0;
-var snakeBody = []
+// Snake variables
+var snakeX;
+var snakeY;
+var velocityX;
+var velocityY;
+var snakeBody;
 
-// Food
+// Food variables
 var foodX;
 var foodY;
 
 // Game variables
 var gameOver;
 var score;
+var updateGameInteralId;
 
 
 window.onload = startGame;
@@ -33,10 +34,14 @@ function startGame() {
     context = board.getContext("2d");
     gameOver = false;
     score = 0;
-    document.getElementById("snake-score").innerHTML = "Score: " + score;
     placeFood();
-    document.addEventListener("keyup", changeDirection);
-    setInterval(update, 100);
+    snakeX = blocksize * 5;
+    snakeY = blocksize * 5;
+    velocityX = 0;
+    velocityY = 0;
+    snakeBody = []
+    document.addEventListener("keyup", respondKeyPress);
+    updateGameInteralId = setInterval(update, 100);
 }
 
 function update() {
@@ -46,10 +51,7 @@ function update() {
 
     // Gameover
     if (gameOver) {
-        context.font = "64px Courier New";
-        context.fillStyle = "red";
-        context.textAlign = "center";
-        context.fillText("Game Over", board.width/2, board.height/2);
+        gameOverState();
         return;
     }
 
@@ -59,11 +61,23 @@ function update() {
     // Food
     updateFood();
 
+    // Score
+    updateScore();
+
     // Check for gameover conditions
     checkGameOver();
 }
 
-function changeDirection(e) {
+function respondKeyPress(e) {
+    // Currently in game over state
+    if (gameOver) {
+        // Retry button pressed
+        if (e.code == "Space") {
+            startGame();
+        }
+        return;
+    }
+
     // Move up
     if (e.code == "KeyW" && velocityY != 1) {
         velocityX = 0;
@@ -120,7 +134,6 @@ function updateFood() {
         snakeBody.push([snakeX - velocityX, snakeY - velocityY])
         placeFood();
         score++;
-        document.getElementById("snake-score").innerHTML = "Score: " + score;
     }
 
     // Draws food
@@ -140,4 +153,23 @@ function checkGameOver() {
             gameOver = true;
         }
     }
+}
+
+function gameOverState() {
+    // Display gameover screen and stop running the game
+    context.font = "64px Courier New";
+    context.fillStyle = "red";
+    context.textAlign = "center";
+    context.fillText("Game Over", board.width/2, board.height/2);
+    context.font = "24px Courier New";
+    context.fillText("Press 'space' to retry", board.width/2, board.height/2 + 32);
+    updateScore();
+    clearInterval(updateGameInteralId);
+}
+
+function updateScore() {
+    context.font = "15px Courier New";
+    context.fillStyle = "white";
+    context.textAlign = "left";
+    context.fillText("Score: " + score, 10, 20);
 }
