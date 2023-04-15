6068655e1be86dd837ff30149d26c268ec44405d
benngim <102727152+benngim@users.noreply.github.com>
2023-04-15 17:05:48 +1000

    Added functionality to check for winner

--- static/js/tictactoe.js
+++ static/js/tictactoe.js
@@ -1,8 +1,23 @@
+/* Implements tictactoe game */
+
+
+/* Game variables */
 var board;
 var gameover;
 var player1 = 'O';
 var player2 = 'X';
 var curPlayer = player1;
+
+/* Win Patterns of Board */
+var winpatterns = [
+    /* Horizontal Win Patterns */
+    [0, 1, 2], [3, 4, 5], [6, 7, 8],
+    /* Vertical Win Patterns */
+    [0, 3, 6], [1, 4, 7], [2, 5, 8],
+    /* Diagonal Win Patterns */
+    [0, 4, 8], [2, 4, 6]
+]
+
 
 window.onload = initialiseGame;
 
@@ -10,7 +25,6 @@ window.onload = initialiseGame;
 function initialiseGame() {
     gameover = false;
     board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '];
-    console.log(board.length)
 
     for (let i = 0; i < board.length; i++) {
         let tile = document.getElementById("tile-"+i);
@@ -34,10 +48,14 @@ function clickTile() {
     }
 
 
-    /* Update board and curplayer */
+    /* Update board */
     board[tilenum] = curPlayer;
     this.innerHTML = curPlayer;
 
+    /* Check if current player won */
+    checkWinner();
+
+    /* Switch to next player */
     if (curPlayer == player1) {
         curPlayer = player2;
     }
@@ -46,11 +64,18 @@ function clickTile() {
         curPlayer = player1;
     }
 
-    checkWinner();
 }
 
 /* Check for winner */
 function checkWinner() {
-    return;
+    for (let i = 0; i < winpatterns.length; i++) {
+        console.log("i = "+ i + " board: " + board[winpatterns[i][0]] + board[winpatterns[i][1]] + board[winpatterns[i][2]])
+        if ((board[winpatterns[i][0]] == board[winpatterns[i][1]]) &&
+            (board[winpatterns[i][1]] == board[winpatterns[i][2]]) &&
+            (board[winpatterns[i][2]] == curPlayer)) {
+            gameover = true;
+            return;
+        }
+    }
 }
 
