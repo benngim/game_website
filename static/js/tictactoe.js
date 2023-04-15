e6284fe3901e87e96ee12783e3599eae0f248344
benngim <102727152+benngim@users.noreply.github.com>
2023-04-15 17:57:08 +1000

    Added color to tictactoe board

--- static/js/tictactoe.js
+++ static/js/tictactoe.js
@@ -8,6 +8,14 @@ var player1 = 'O';
 var player2 = 'X';
 var curPlayer = player1;
 
+/* Color variable */
+var player1_col = 'red';
+var player1_wincol = 'darkred';
+var player2_col = 'blue';
+var player2_wincol = 'darkblue';
+var board_color = 'black';
+var line_color = 'lime';
+
 /* Win Patterns of Board */
 var winpatterns = [
     /* Horizontal Win Patterns */
@@ -16,7 +24,7 @@ var winpatterns = [
     [0, 3, 6], [1, 4, 7], [2, 5, 8],
     /* Diagonal Win Patterns */
     [0, 4, 8], [2, 4, 6]
-]
+];
 
 
 window.onload = initialiseGame;
@@ -25,6 +33,7 @@ window.onload = initialiseGame;
 function initialiseGame() {
     gameover = false;
     board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '];
+    setTheme();
 
     for (let i = 0; i < board.length; i++) {
         let tile = document.getElementById("tile-"+i);
@@ -50,6 +59,14 @@ function clickTile() {
 
     /* Update board */
     board[tilenum] = curPlayer;
+    var current_color;
+    if (player1 == curPlayer) {
+        current_color = player1_col;
+    }
+    else {
+        current_color = player2_col;
+    }
+    this.style.color = current_color;
     this.innerHTML = curPlayer;
 
     /* Check if current player won */
@@ -74,8 +91,26 @@ function checkWinner() {
             (board[winpatterns[i][1]] == board[winpatterns[i][2]]) &&
             (board[winpatterns[i][2]] == curPlayer)) {
             gameover = true;
+            var win_color;
+            if (player1 == curPlayer) {
+                win_color = player1_wincol;
+            }
+            else {
+                win_color = player2_wincol;
+            }
+            document.getElementById("tile-"+[winpatterns[i][0]]).style.backgroundColor = win_color;
+            document.getElementById("tile-"+[winpatterns[i][1]]).style.backgroundColor = win_color;
+            document.getElementById("tile-"+[winpatterns[i][2]]).style.backgroundColor = win_color;
             return;
         }
     }
+}
+
+/* Sets game color theme */
+function setTheme() {
+    document.querySelectorAll('.tictactoe-tile, .tictactoe-board').forEach(tile => {
+        tile.style.backgroundColor = board_color;
+        tile.style.borderColor = line_color;
+    });        
 }
 
