07a45265da74966b9fd1d98507b76587d0b0515a
benngim <102727152+benngim@users.noreply.github.com>
2023-04-16 14:35:25 +1000

    Computer now uses minimax for choosing moves

--- static/js/tictactoe.js
+++ static/js/tictactoe.js
@@ -93,13 +93,22 @@ function playGameCPU() {
 /* Cpu places a move */
 function cpuMove() {
     clickable = true;
-    let tileid = getRandomMove();
+    /* let tileid = getRandomMove();*/
+    move = minimaxMove(3, true, board);
+    let tileid = "tile-" + move[1];
     tile = document.getElementById(tileid).click();
 }
 
 /* Chooses a random move from remaining valid moves */
 function getRandomMove() {
-    /* Get list of all valid moves */
+    let valid_moves = getValidMoves(board);
+    let move = valid_moves[Math.floor(Math.random() * valid_moves.length)];
+
+    return "tile-" + move;
+}
+
+/* Get all remaining valid moves */
+function getValidMoves(board) {
     let valid_moves = [];
     for (let i = 0; i < board.length; i++) {
         if (board[i] == " ") {
@@ -107,9 +116,93 @@ function getRandomMove() {
         }
     }
 
-    let move = valid_moves[Math.floor(Math.random() * valid_moves.length)];
+    return valid_moves;
+}
 
-    return "tile-" + move;
+/* Returns copy of board */
+function copy_board(board) {
+    let board_copy = [];
+    for (let i = 0; i < board.length; i++) {
+        if (board[i] == " ") {
+            board_copy.push(" ");
+        }
+        else if (board[i] == player1) {
+            board_copy.push(player1);
+        }
+        else {
+            board_copy.push(player2);
+        }
+    }
+
+    return board_copy;
+}
+
+/* Use minmax algorithm to search for move based on given depth */
+function minimaxMove(depth, maximising, board) {
+    let valid_moves = getValidMoves(board);
+    let best_evaluation = 2;
+    if (maximising) {
+        best_evaluation = -2;
+    }
+    let best_move = valid_moves[0];
+    let board_copy = copy_board(board);
+
+    for (let i=0; i < valid_moves.length; i++) {
+        if (maximising) {
+            cur_evaluation = evaluateMove(board_copy, valid_moves[i], curPlayer);
+            if (cur_evaluation == 1) {
+                return [cur_evaluation, valid_moves[i]];
+            }
+            if (depth != 1 && (valid_moves.length > 1)) {
+                board_copy[valid_moves[i]] = curPlayer;
+                cur_evaluation = minimaxMove(depth - 1, false, board_copy)[0];
+                board_copy[valid_moves[i]] = " ";
+            }
+            if (cur_evaluation >= best_evaluation) {
+                best_evaluation = cur_evaluation;
+                best_move = valid_moves[i];
+            }
+        }
+        else {
+            if (curPlayer == player1) {
+                opp_player = player2;
+            }
+            else {
+                opp_player = player1;
+            }
+            cur_evaluation = -1 * evaluateMove(board_copy, valid_moves[i], opp_player);
+            if (cur_evaluation == -1) {
+                return [cur_evaluation, valid_moves[i]];
+            }
+            if (depth != 1 && (valid_moves.length > 1)) {
+                board_copy[valid_moves[i]] = opp_player;
+                cur_evaluation = minimaxMove(depth - 1, true, board_copy)[0];
+                board_copy[valid_moves[i]] = " ";
+            }
+            if (cur_evaluation <= best_evaluation) {
+                best_evaluation = cur_evaluation;
+                best_move = valid_moves[i];
+            }
+
+        }
+    }
+
+    return [best_evaluation, best_move];
+}
+
+/* Simple valuation function for minimax */
+function evaluateMove(board, move, player) {
+    board[move] = player;
+
+    /* Evaluation is 1 for win, 0 for anything else */
+    if (checkWinner(board, player) >= 0) {
+        evaluation = 1;
+    }
+    else {
+        evaluation = 0;
+    }
+    board[move] = " ";
+    return evaluation;
 }
 
 /* Action when a tile is clicked */
@@ -139,10 +232,29 @@ function makeMove() {
     this.style.color = current_color;
     this.innerHTML = curPlayer;
 
-    /* Check if current player won or if there is a tie */
-    checkWinner();
-    checkTie();
-    if (gameover) {
+    /* Check if current player won */
+    let winner = checkWinner(board, curPlayer);
+    if (winner >= 0) {
+        gameover = true;
+        var win_color;
+        if (player1 == curPlayer) {
+            win_color = player1_wincol;
+            document.getElementById("gamelog").innerHTML = "Player 1 Wins";
+        }
+        else {
+            win_color = player2_wincol;
+            document.getElementById("gamelog").innerHTML = "Player 2 Wins";
+        }
+        document.getElementById("tile-"+[winpatterns[winner][0]]).style.backgroundColor = win_color;
+        document.getElementById("tile-"+[winpatterns[winner][1]]).style.backgroundColor = win_color;
+        document.getElementById("tile-"+[winpatterns[winner][2]]).style.backgroundColor = win_color;
+        return;
+    }
+
+    /* Check if there is a tie */
+    if (checkTie(board)) {
+        gameover = true;
+        document.getElementById("gamelog").innerHTML = "Draw";
         return;
     }
 
@@ -173,43 +285,25 @@ function makeMove() {
 }
 
 /* Check for winner */
-function checkWinner() {
+function checkWinner(board, player) {
     for (let i = 0; i < winpatterns.length; i++) {
         if ((board[winpatterns[i][0]] == board[winpatterns[i][1]]) &&
             (board[winpatterns[i][1]] == board[winpatterns[i][2]]) &&
-            (board[winpatterns[i][2]] == curPlayer)) {
-            gameover = true;
-            var win_color;
-            if (player1 == curPlayer) {
-                win_color = player1_wincol;
-                document.getElementById("gamelog").innerHTML = "Player 1 Wins";
-            }
-            else {
-                win_color = player2_wincol;
-                document.getElementById("gamelog").innerHTML = "Player 2 Wins";
-            }
-            document.getElementById("tile-"+[winpatterns[i][0]]).style.backgroundColor = win_color;
-            document.getElementById("tile-"+[winpatterns[i][1]]).style.backgroundColor = win_color;
-            document.getElementById("tile-"+[winpatterns[i][2]]).style.backgroundColor = win_color;
-            return;
+            (board[winpatterns[i][2]] == player)) {
+            return i;
         }
     }
+    return -1;
 }
 
 /* Check if game ended in tie */
-function checkTie() {
-    if (gameover) {
-        return;
-    }
-
+function checkTie(board) {
     for (let i = 0; i < board.length; i++) {
         if (board[i] == " ") {
-            return;
+            return false;
         }
     }
-    gameover = true;
-    document.getElementById("gamelog").innerHTML = "Draw";
-    return;
+    return true;
 }
 
 /* Sets game color theme */
