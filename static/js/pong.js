79cd29017294a3bc20b3377912aa90e5400e4c86
benngim <102727152+benngim@users.noreply.github.com>
2023-04-17 16:10:05 +1000

    Added ability to move paddles

--- static/js/pong.js
+++ static/js/pong.js
@@ -13,8 +13,10 @@ var gameOver = false;
 // Player variables
 var p1_x;
 var p1_y;
+var p1_velocity;
 var p2_x;
 var p2_y;
+var p2_velocity;
 
 // Ball variables
 var ball_x;
@@ -40,14 +42,19 @@ function initialiseGame() {
     // Initialise position variables
     p1_x = 2 * BLOCKSIZE;
     p1_y = (ROWS/2 - 2) * BLOCKSIZE;
+    p1_velocity = 0;
     p2_x = (COLS - 2) * BLOCKSIZE;
     p2_y = (ROWS/2 - 2) * BLOCKSIZE;
+    p2_velocity = 0;
     ball_x = (COLS/2 + 1) * BLOCKSIZE;
     ball_y = (ROWS/2 - 1) * BLOCKSIZE;
 
+    // Adds event listener for keyboard press and release
+    document.addEventListener("keydown", respondKeyPress);
+    document.addEventListener("keyup", respondKeyUp);
+
     // Draws game elements
     drawGame();
-
 }
 
 function drawGame() {
@@ -75,4 +82,71 @@ function drawGame() {
     // Draw ball
     context.fillStyle = ball_color;
     context.fillRect(ball_x, ball_y, BALLSIZE, BALLSIZE);
+}
+
+function respondKeyPress(e) {
+    if (gameOver) {
+        return;
+    }
+
+    // Move P1 Paddle
+    if (e.code == "KeyW") {
+        p1_velocity = -1;
+    }
+    else if (e.code == "KeyS") {
+        p1_velocity = 1;
+    }
+    // Move P2 Paddle
+    else if (e.code == "KeyI") {
+        p2_velocity = -1;
+    }
+    else if (e.code == "KeyK") {
+        p2_velocity = 1;
+    }
+}
+
+function respondKeyUp(e) {
+    // Stop paddle movement up key press is released
+    if (e.code == "KeyW" || e.code == "KeyS") {
+        p1_velocity = 0;
+    }
+    else if (e.code == "KeyI" || e.code == "KeyK") {
+        p2_velocity = 0;
+    }
+}
+
+function updatePaddles() {
+    // Update paddle positions
+    p1_y += p1_velocity * BLOCKSIZE/4;
+    p2_y += p2_velocity * BLOCKSIZE/4;
+
+    // Ensure paddles don't go out of bound
+    if (p1_y <= 0) {
+        p1_y = 0;
+    }
+    else if(p1_y >= board.height - (BLOCKSIZE * PADDLE_SIZE)) {
+        p1_y = board.height - (BLOCKSIZE * PADDLE_SIZE);
+    }
+    if (p2_y <= 0) {
+        p2_y = 0;
+    }
+    else if(p2_y >= board.height - (BLOCKSIZE * PADDLE_SIZE)) {
+        p2_y = board.height - (BLOCKSIZE * PADDLE_SIZE);
+    }
+}
+
+// Start game
+function startGame() {
+    initialiseGame();
+    gameOver = false;
+    requestAnimationFrame(playGame);
+}
+
+// Play game
+function playGame() {
+    updatePaddles();
+    drawGame();
+    if (!gameOver) {
+        requestAnimationFrame(playGame);
+    }
 }
