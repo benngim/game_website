e510241a1f5d2f255c94fd9ac9860f86622024a3
benngim <102727152+benngim@users.noreply.github.com>
2023-05-05 02:15:17 +1000

    Created register page

--- app.py
+++ app.py
@@ -52,6 +52,10 @@ def index():
 def login():
     return render_template("login.html")
 
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    return render_template("register.html")
+
 @app.route("/snake")
 def snake():
     return render_template("snake.html")
