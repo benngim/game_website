610457647f04956a1e3afd4124d148c53197a0bd
benngim <102727152+benngim@users.noreply.github.com>
2023-05-05 11:05:54 +1000

    Completed register functionality

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 
 from flask import Flask, render_template, redirect, request, session
 import mysql.connector
+import re
 import os
 from dotenv import load_dotenv
 
@@ -18,7 +19,7 @@ mydb = mysql.connector.connect(
     password = ""
 )
 
-mycursor = mydb.cursor()
+mycursor = mydb.cursor(dictionary=True)
 mycursor.execute("CREATE DATABASE IF NOT EXISTS websitedb")
 mycursor.execute("USE websitedb")
 mycursor.execute("""CREATE TABLE IF NOT EXISTS locations (
@@ -72,7 +73,7 @@ def login():
         # Account exists, log in and redirect to home page
         if account:
             session["loggedin"] = True
-            session["id"] = account["id"]
+            session["id"] = account["account_id"]
             session["username"] = account["username"]
             return redirect("/")
         # Username and password doesn't match any account in database
@@ -83,7 +84,70 @@ def login():
 
 @app.route("/register", methods=['GET', 'POST'])
 def register():
-    return render_template("register.html")
+    status_msg = ""
+    if request.method == "POST" and "username" in request.form and "password" in request.form\
+    and "email" in request.form and "age" in request.form and "country" in request.form\
+    and "state" in request.form and "city" in request.form:
+        username = request.form["username"]
+        password = request.form["password"]
+        email = request.form["email"]
+        age = request.form["age"]
+        country = request.form["country"].lower()
+        state = request.form["state"].lower()
+        city = request.form["city"].lower()
+
+        # Check if account is already in database
+        mycursor.execute(
+            """SELECT * 
+            FROM accounts 
+            WHERE username = %s""",
+            (username, ))
+        account = mycursor.fetchone()
+        if account:
+            status_msg = "Username is already taken!"
+        elif not re.match(r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,7}\b', email):
+            status_msg = "Invalid email!"
+        elif not re.match(r'[A-Za-z0-9]+', username):
+            status_msg = "Username can only contain numbers or letters!"
+        elif not re.match(r'[0-9]+', age):
+            status_msg = "Please enter a valid age!"
+        else:
+            mycursor.execute(
+                """SELECT *
+                FROM locations
+                WHERE country = %s
+                AND state = %s
+                AND city = %s""",
+                (country, state, city)
+            )
+            location = mycursor.fetchone()
+            if not location:
+                mycursor.execute(
+                    """INSERT INTO locations VALUES
+                    (NULL, %s, %s, %s)
+                    """,
+                    (country, state, city)
+                )
+                mydb.commit()
+                mycursor.execute(
+                    """SELECT *
+                    FROM locations
+                    WHERE country = %s
+                    AND state = %s
+                    AND city = %s""",
+                    (country, state, city)
+                )
+                location = mycursor.fetchone()
+            mycursor.execute(
+                """INSERT INTO accounts VALUES
+                (NULL, %s, %s, %s, %s, %s)
+                """,
+                (username, password, email, age, location["location_id"])
+            )
+            mydb.commit()
+            status_msg = "You have successfuly registered an account!"
+
+    return render_template("register.html", msg = status_msg)
 
 @app.route("/snake")
 def snake():
